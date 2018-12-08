96e51a9862ae6289c5cdf97c26e64b49e186cf0c
Paula Peroutka <paulaperoutka@gmail.com>
2018-12-07 23:52:10 -0500

    add sample Navbar code for navbar span; CHANGE CLASSNAMES LATER

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Navbar = (props) => (
-  <nav className="nav"></nav>
+  <nav className="nav">
+    <span className="nav-span">SAMPLE NAV CODE</span>
+  </nav>
 );
 
 export default Navbar;
