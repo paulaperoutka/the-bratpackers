ff06b15df442e10562907e8b7b940b9503c4e52e
Paula Peroutka <paulaperoutka@gmail.com>
2018-12-09 23:59:55 -0500

    add Footer code

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = (props) => (
+  <div className="foot">
+    <span className="footer-span">SAMPLE FOOTER CODE</span>
+  </div>
+)
+
+export default Footer; 
