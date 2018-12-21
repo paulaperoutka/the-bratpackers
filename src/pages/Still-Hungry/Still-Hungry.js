7c3a7c936f3e2b576e25e95d911ee530b2c8a1be
Paula Peroutka <paulaperoutka@gmail.com>
2018-12-20 23:37:59 -0500

    add still hungry div and update travel div

--- src/pages/Still-Hungry/Still-Hungry.js
+++ src/pages/Still-Hungry/Still-Hungry.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Travel = (props) => (
+const Hungry = (props) => (
   <div />
 )
 
--- src/pages/Travel-To/Travel-To.js
+++ src/pages/Travel-To/Travel-To.js
@@ -1,5 +1,7 @@
 import React from "react";
 
-
+const Travel = (props) => (
+  <div />
+)
 
 export default Travel;
