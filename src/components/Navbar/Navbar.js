28c87f17766e0b3a1e7a09a2f091f80bc6ba8351
Paula Peroutka <paulaperoutka@gmail.com>
2018-12-26 23:52:29 -0500

    add div nav title to nav component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,7 +2,8 @@ import React from "react";
 
 const Navbar = (props) => (
   <nav className="nav">
-    <span className="nav-span">SAMPLE NAV CODE</span>
+    {/* <span className="nav-span">SAMPLE NAV CODE</span> */}
+    <div class="nav-title">The Bratpackers</div>
   </nav>
 );
 
