50e8a52e0c125ba90a3883d3d3e67435c158f876
Paula Peroutka <paulaperoutka@gmail.com>
2018-12-27 23:51:10 -0500

    correct class to classname

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = (props) => (
   <nav className="nav">
     {/* <span className="nav-span">SAMPLE NAV CODE</span> */}
-    <div class="nav-title">The Bratpackers</div>
+    <div className ="nav-title">The Bratpackers</div>
   </nav>
 );
 
